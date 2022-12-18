ed48b5c914bee2c4c223464d0862c9bc15e37193
Bakoulis George <115095653+bak-george@users.noreply.github.com>
2022-12-18 20:19:21 +0200

    Campground Index

--- javascript/YelpCamp/app.js
+++ javascript/YelpCamp/app.js
@@ -3,10 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -23,12 +20,11 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: 'My Backyard', description: 'test camp'});
-    await camp.save(); 
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index');
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+app.listen(5500, () => {
+    console.log('Serving on port 5500')
 })
